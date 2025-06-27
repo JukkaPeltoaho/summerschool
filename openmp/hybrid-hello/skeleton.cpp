2336998d19d2e01fbbd12de4704d1acaef5b81bd
Jukka Pelto-aho <jukka.peltoaho@gmail.com>
2025-06-27 15:46:08 +0300

    hdev16

--- openmp/hybrid-hello/skeleton.cpp
+++ openmp/hybrid-hello/skeleton.cpp
@@ -8,9 +8,18 @@ int main(int argc, char *argv[])
     int provided, required=MPI_THREAD_FUNNELED;
 
     /* TODO: Initialize MPI with thread support. */
+    MPI_Init_thread(&argc, &argv, required, &provided);
+    MPI_Comm_rank(MPI_COMM_WORLD, &omp_rank);
+
 
     /* TODO: Find out the MPI rank and thread ID of each thread and print
      *       out the results. */
+
+#pragma omp parallel private(my_id)
+    {
+        my_id = omp_get_thread_num();
+        printf("I'm thread %d in process %d\n", my_id, omp_rank);
+    }
 
     /* TODO: Investigate the provided thread support level. */
 
