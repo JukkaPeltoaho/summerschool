41922c67b91d927261ad66dc045b2320cb480d9a
Jukka Pelto-aho <jukka.peltoaho@gmail.com>
2025-06-27 10:39:41 +0300

    hdev11

--- openmp/omp-hello/hello.cpp
+++ openmp/omp-hello/hello.cpp
@@ -1,11 +1,17 @@
 #include <cstdio>
 #include <omp.h>
+#include <mpi.h>
+
 int main()
 {
     printf("Hello world!\n");
+	int omp_rank, num_threads;
 #pragma omp parallel
     {
-        printf("X\n");
+		omp_rank = omp_get_thread_num();
+		num_threads = omp_get_num_threads();
+        printf("X: %d\n", omp_rank);
     }
+	printf("Threadnum: %d\n", num_threads);
     return 0;
 }
