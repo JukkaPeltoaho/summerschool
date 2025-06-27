9bbec36f252284009d569c10260184ca292c58b8
Jukka Pelto-aho <jukka.peltoaho@gmail.com>
2025-06-27 14:52:04 +0300

    dev15

--- openmp/simple-tasks/tasks.cpp
+++ openmp/simple-tasks/tasks.cpp
@@ -14,13 +14,16 @@ int main(void)
 
     // TODO: launch threads and create tasks so that there 
     // one task per loop iteration
-#pragma omp parallel
-#pragma omp single
+#pragma omp parallel private(tid)
+#pragma omp single 
 {
     for (int i=0; i < 4; i++) {
-           tid = omp_get_thread_num();
+        #pragma omp task
+	{
+      	   tid = omp_get_thread_num();
            printf("Task %d executed by thread %d\n", i, tid);
            array[i] += tid;
+	}
     }
 }
     // TODO end
